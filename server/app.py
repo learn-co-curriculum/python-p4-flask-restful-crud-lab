6555c6bd374574bb68aa65c7b520567cf1ecbeb0
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-31 09:31:15 -0500

    fix db issue

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
 
