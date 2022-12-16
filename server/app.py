8d4bc330824be5a163deeae7c47a86fecb9b30db
Ben Botsford <ben.botsford@flatironschool.com>
2022-12-16 14:42:57 -0600

    update for pytest

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -49,4 +49,4 @@ api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5555, debug=True)
